3d36ad30d55b13001aa42dd7e8f63d723dc0ad07
sadsirko <48710082+sadsirko@users.noreply.github.com>
2022-01-23 22:49:15 +0200

    update server

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ io.on('connection', (socket) => {
 
 // for deploy
 const publicPath = path.join(__dirname, 'build');
-
+app.use(express.static(publicPath));
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
